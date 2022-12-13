8388fad5b2a5dd31320cfc5f0974768e1ad32ee2
aklsjd <nabiya0876@naver.com>
2022-12-13 12:28:42 +0900

    todo 삭제 버튼 생성

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,19 +1,32 @@
-// 1. 함수형 컴포넌트
-// 2. input(checkbox) 와 label을 렌더링하는 컴포넌트!
-// 3. App (부모 컴포넌트)에서 Todo (자식 컴포넌트) 1개를 렌더링
-// import "./Todo.css";
-const Todo = ({ todo }) => {
+import { useState } from "react";
+
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
+  const { id, title, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
+
+  // title input 커서가 깜빡인다고 수정이 가능한 것은 아님
+  // 사용자가 키보드 입력할 때마다 todoItem의 title을 새 값으로 변경
+  const editEventHandler = (e) => {};
+
   return (
-    <div className={todo.title}>
+    <div className="Todo">
       <input
         type="checkbox"
-        name={todo.title}
-        id={todo.title}
-        value={todo.title}
-        defaultChecked={todo.done}
+        id={`todo${id}`}
+        name={`todo${id}`}
+        value={`todo${id}`}
+        defaultChecked={done}
       />
-      <label htmlFor={todo.title}>{todo.title}</label>
+      {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+      <input type="text" value={todoItem.title} onChange={editEventHandler} />
+      <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   );
 };
+
 export default Todo;
